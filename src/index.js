4244c5bfe86ecc7c5773e0ed7ecb3f281e454657
Alba Giraldo <agiraldomolina@gmail.com>
2023-06-07 21:24:15 -0400

    con captura de errores en inputs ok

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const set1 = document.querySelector("#input-set1");
 const set2 = document.querySelector("#input-set2");
 const set3 = document.querySelector("#input-set3");
 const set4 = document.querySelector("#input-set4");
-console.log(set1);
+
+const month = document.querySelector("#input-month");
+const year = document.querySelector("#input-year");
+const CCV = document.querySelector("#input-cvc");
 
 const monthlyDonation = document.querySelector("#monthly-donation");
 const onceDonation = document.querySelector("#once-donation");
@@ -19,6 +22,9 @@ const buttonVerify = document.querySelector(".button-verify");
 
 let donationKind = "";
 let amountDonated = 0;
+let monthOk = false;
+let yearOk = false;
+let ccvOk = false;
 
 monthlyDonation.addEventListener("click", function () {
   donationKind = "mensual";
@@ -56,30 +62,107 @@ set1.addEventListener("keypress", function (e) {
   if (onlyDigits(e) === false) {
     e.preventDefault();
   }
+  if (set1.value.length === 4) set2.focus();
 });
 
 set2.addEventListener("keypress", function (e) {
   if (onlyDigits(e) === false) {
     e.preventDefault();
   }
+  if (set2.value.length === 4) set3.focus();
 });
 
 set3.addEventListener("keypress", function (e) {
   if (onlyDigits(e) === false) {
     e.preventDefault();
   }
+  if (set3.value.length === 4) set4.focus();
 });
 
 set4.addEventListener("keypress", function (e) {
   if (onlyDigits(e) === false) {
     e.preventDefault();
   }
+  if (set4.value.length === 4) month.focus();
+});
+
+month.addEventListener("keypress", function (e) {
+  if (onlyDigits(e) === false) {
+    e.preventDefault();
+  }
+  if (month.value.length === 2) {
+    if (+month.value > 0 && +month.value < 13) {
+      monthOk = true;
+      year.focus();
+    } else {
+      monthOk = false;
+      alert(`${month.value} no es un número de mes válido`);
+      month.value = "";
+      month.focus();
+    }
+  }
+});
+
+year.addEventListener("keypress", function (e) {
+  if (onlyDigits(e) === false) {
+    e.preventDefault();
+  }
+  if (year.value.length === 2) {
+    if (+year.value > 22 && +year.value < 28) {
+      yearOk = true;
+      CCV.focus();
+    } else {
+      yearOk = false;
+      alert(`${year.value} no es un número de añ0 válido`);
+      year.value = "";
+      year.focus();
+    }
+  }
+});
+
+CCV.addEventListener("keypress", function (e) {
+  if (onlyDigits(e) === false) {
+    e.preventDefault();
+  }
+  if (CCV.value.length === 3) {
+    if (+CCV.value > 0 && +CCV.value < 1000) {
+      ccvOk = true;
+      buttonVerify.focus();
+    } else {
+      ccvOk = false;
+      alert(`${CCV} no es un código de seguridad válido`);
+      CCV.value = "";
+      CCV.focus();
+    }
+  }
 });
 
 buttonVerify.addEventListener("click", function () {
   alert(
-    `Muchas gracias por hacer tu donación ${donationKind} por un valor de ${amountDonated}`
+    `🐱😻 Muchas gracias por hacer tu donación ${donationKind} por un valor de ${amountDonated}$`
   );
+
+  // if (+month.value > 0 && +month.value < 13) {
+  //   monthOk = true;
+  // } else {
+  //   monthOk = false;
+  //   alert(`${month.value} no es un número de mes válido`);
+  //   month.value = "";
+  //   month.focus();
+  // }
+
+  // if (+year.value > 22 && +year.value < 28) {
+  //   year = true;
+  // } else {
+  //   year = false;
+  //   alert(`${year.value} no es un número de añ0 válido`);
+  //   year.value = "";
+  //   year.focus();
+  // }
+
+  // +year.value > 22 && +year.value < 28 ? (yearOk = true) : (yearOk = false);
+
+  // +CCV.value > 0 && +CCV.value < 1000 ? (ccvOk = true) : (ccvOk = false);
 
   //read digits from inputs
   const digitsSet1 = set1.value;
